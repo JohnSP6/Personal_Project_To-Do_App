a332db08abb7e37a23fe778d0977784922b8fa6a
JohnSP6 <johnsp6@gmail.com>
2025-07-23 22:09:34 +0800

    Removed secret leak

--- To-Do_List_BE/src/main/java/com/jsper/To_Do_List_BE/ToDoListBeApplication.java
+++ To-Do_List_BE/src/main/java/com/jsper/To_Do_List_BE/ToDoListBeApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ToDoListBeApplication {
 
-	//mongodb+srv://admin:MyT0D0App@to-do-cluster.wfv5bap.mongodb.net/
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListBeApplication.class, args);
 	}
